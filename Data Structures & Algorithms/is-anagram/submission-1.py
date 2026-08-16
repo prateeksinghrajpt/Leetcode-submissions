cb6e75b205bcede2e7cad6d2eae49a3dd125f8e4
Prateek Singh <147691011+prateeksinghrajpt@users.noreply.github.com>
2026-08-16 12:37:01 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            return sorted(s) == sorted(t)
+        else:
+            return False 
+       
